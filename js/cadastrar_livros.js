184343270a81e8247084b4d5fbba8f251544c0dd
educacaoprimeiro2025 <educacaoprimeiro2025@gmail.com>
2025-11-11 11:43:00 -0300

    Add files via upload

--- js/cadastrar_livros.js
+++ js/cadastrar_livros.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qzsmrnbpawbqdqeezqua.supabase.co";
+const SUPABASE_URL = "https://qzsmrnbpawbydqeezqua.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c21ybmJwYXdieWRxZWV6cXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MDM0NTAsImV4cCI6MjA3ODM3OTQ1MH0.HwOSk4_qtfRKLjYeO1o0e4qyXULxDRM7NSwzy2xvSoQ";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
