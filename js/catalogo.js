f43cec1fb5f1807b51406e93bf60ba0a79c707ca
educacaoprimeiro2025 <educacaoprimeiro2025@gmail.com>
2025-11-11 12:51:59 -0300

    catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -3,11 +3,10 @@
 // ==============================
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://qzsmrnbpawbydqeezqua.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c21ybmJwYXdieWRxZWV6cXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MDM0NTAsImV4cCI6MjA3ODM3OTQ1MH0.HwOSk4_qtfRKLjYeO1o0e4qyXULxDRM7NSwzy2xvSoQ";
+const supabaseUrl = "https://qzsmrnbpawbydqeezqua.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c21ybmJwYXdieWRxZWV6cXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MDM0NTAsImV4cCI6MjA3ODM3OTQ1MH0.HwOSk4_qtfRKLjYeO1o0e4qyXULxDRM7NSwzy2xvSoQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ==============================
 // 📚 CÓDIGO PRINCIPAL
@@ -133,4 +132,5 @@ if (btnLogout) {
     localStorage.clear();
     window.location.href = "../login/login.html";
   });
+
 }
