87b1b87d94b67d1cb778ad24619091b2f4ed84a1
educacaoprimeiro2025 <educacaoprimeiro2025@gmail.com>
2025-11-12 09:15:58 -0300

    Update carrossel2.js

--- js/carrossel2.js
+++ js/carrossel2.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // 🔹 Substitua com suas credenciais do Supabase
 const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA-CHAVE-ANON";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6c21ybmJwYXdieWRxZWV6cXVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MDM0NTAsImV4cCI6MjA3ODM3OTQ1MH0.HwOSk4_qtfRKLjYeO1o0e4qyXULxDRM7NSwzy2xvSoQ";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const carrossel = document.getElementById("carrosselLivros");
@@ -53,5 +53,6 @@ async function carregarLivros() {
 }
 
 carregarLivros();
+
 
 
